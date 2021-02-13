1921769dfd12cd243cec5d193877132696084c22
Robson Cristian de Lira <robso.crlira@gmail.com>
2021-02-13 09:47:57 -0300

    Finalizando a introducao a Jquery
    Finalização da introdução a JQuery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-function consultaCEP(){
+function consultaCEP() {
+    $(".bar-progress").show();
     var cep = document.getElementById("cep").value;
-    var url = "https://viacep.com.br/ws/"+ cep +"/json/";
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
     $.ajax({
         url: url,
         type: "GET",
-        success: function(response){
+        success: function (response) {
             console.log(response);
+            $("#titulo").html("CEP: " + response.cep);
+            $("#logradouro").html(response.logradouro);
+            $("#complemento").html(response.complemento);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#ibge").html(response.ibge);
+            $("#gia").html(response.gia);
+            $("#ddd").html(response.ddd);
+            $("#siafi").html(response.siafi);
+            $(".cep").show();
+            $(".bar-progress").hide();
         }
-    })
+    });
 }
+
+
+$(function () {
+    $(".cep").hide();
+    $(".bar-progress").hide();
+});
